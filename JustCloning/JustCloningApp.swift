2811810d269044f31c9a3a7c02b0437144791085
Lorenzo Tarabelli <tarabelli38@gmail.com>
2024-11-04 15:35:21 +0100

    Initial Commit

---
+++ JustCloning/JustCloningApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustCloningApp.swift
+//  JustCloning
+//
+//  Created by Lorenzo Tarabelli on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct JustCloningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
